5d5e9c8f147ca2a0f92ca4822d30fcc8210134d2
duankunh <D2446925718Kh@gmail.com>
2024-03-12 23:57:46 -0400

    edit urls.py

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     path('meetings/<int:id>/proposals/', views.schedule_proposals),
     path('meetings/<int:id>/finalized/', views.schedule_get_finalize),
     path('meetings/<int:meeting_id>/finalized/<int:schedule_id>/', views.schedule_make_finalize)
-
-
 ]
